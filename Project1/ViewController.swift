eb4348e7b4a39baf5d9b98fd63c7988bdc97cd5e
Ami <foreverazo2013@gmail.com>
2019-10-28 10:06:20 +0100

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Mati on 28/10/2019.
+//  Copyright © 2019 Mati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
